d7a0d9dc96c36faee01d019f0a32cb163d2638e0
NoahVandy <nvandervel@my.gcu.edu>
2022-03-06 16:21:09 -0600

    added start prompt

--- Index.js
+++ Index.js
@@ -1,1 +1,36 @@
 const inquirer = require("inquirer")
+const employees = []
+const Employee = require("./lib/Employee.js")
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineer.js")
+
+function startPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "type",
+      message:
+        "Would you like to add an Engineer or an Intern to this project?",
+      choices: ["Engineer", "Intern"],
+    })
+    .then((answer) => {
+      if (answer.type === "Engineer") {
+        engineerPrompt().then((object) => {
+          let { name, ID, email, github } = object
+          employees.push(new Engineer(name, ID, email, github))
+          return finishPrompt()
+        })
+      } else {
+        internPrompt().then((object) => {
+          let { name, ID, email, school } = object
+          memberList.push(new Intern(name, ID, email, school))
+          return finishPrompt()
+        })
+      }
+    })
+}
+
+function internPrompt() {}
+
+function engineerPrompt() {}
+startPrompt()
