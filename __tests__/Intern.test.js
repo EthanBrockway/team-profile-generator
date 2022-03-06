fe75908e7d41908d688bfedc18d3f825e5835e63
NoahVandy <nvandervel@my.gcu.edu>
2022-03-05 19:13:19 -0600

    added Intern constructor and extended it with employee to pass all tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,10 +1,10 @@
 const Intern = require("../lib/Intern")
 
 it("Should return all of the interns information", () => {
-  const intern = new Intern("Dave", 1, "email.com")
+  const intern = new Intern("Dave", 1, "email.com", "GCU")
 
   expect(intern.getName()).toBe("Dave")
   expect(intern.getId()).toBe(1)
   expect(intern.getEmail()).toBe("email.com")
-  expect(intern.getSchool()).toBe("School")
+  expect(intern.getSchool()).toBe("GCU")
 })
